6823680b6b290dd9cb77777e5b57b966e747c1c7
M-Sh-97 <m-sh-97@yandex.ua>
2016-01-21 16:12:39 +0200

    Connection 6.0.0: возврат блокировки при приёме команд (остаётся (!) поддержка одноабзацных версий программ), функционал для отправки файлов.

--- src/Connection.java
+++ src/Connection.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Base64;
 import java.util.NoSuchElementException;
 
 /**
@@ -30,44 +31,49 @@ class Connection {
   }
 
   public Command receive() throws IOException, NoSuchElementException {
-    if (input.ready()) {
-      StringBuilder iit = new StringBuilder();
-      String it;
-      do {
-	it = input.readLine();
-	if (! (it == null) && ! it.isEmpty()) {
-	  iit.append(it);
-	  iit.append(Protocol.endOfLine);
-	}
-      } while (input.ready());
-      return Command.getCommand(iit.toString());
-    }
-    else
-      return null;
+    StringBuilder iit = new StringBuilder();
+    String it;
+    do {
+      it = input.readLine();
+      if (! ((it == null) || it.isEmpty())) {
+	iit.append(it);
+	iit.append(Protocol.endOfLine);
+      }
+    } while (input.ready());
+    return Command.getCommand(iit.toString());
   }
 
   public void accept() throws IOException {
-    output.write(Protocol.acceptionCommandPhrase + Protocol.endOfLine);
+    output.write(Protocol.acceptionCommandPhrase);
+    output.write(Protocol.endOfLine);
     output.flush();
   }
 
   public void reject() throws IOException {
-    output.write(Protocol.rejectionCommandPhrase + Protocol.endOfLine);
+    output.write(Protocol.rejectionCommandPhrase);
+    output.write(Protocol.endOfLine);
     output.flush();
   }
 
-  public void sendNickHello(String nickName) throws IOException {
-    output.write(Protocol.programName + " " + Protocol.version + " " + Protocol.nickCommandPhrase + " " + nickName + Protocol.endOfLine);
-    output.flush();
-  }
-
-  public void sendNickBusy(String nickName) throws IOException {
-    output.write(Protocol.programName + " " + Protocol.version + " " + Protocol.nickCommandPhrase + " " + nickName + " busy" + Protocol.endOfLine);
+  public void sendNick(String nickName, boolean busyStatus) throws IOException {
+    output.write(Protocol.programName);
+    output.write(Protocol.space);
+    output.write(Protocol.version);
+    output.write(Protocol.space);
+    output.write(Protocol.nickCommandPhrase);
+    output.write(Protocol.space);
+    output.write(nickName);
+    if (busyStatus) {
+      output.write(Protocol.space);
+      output.write(Protocol.busyStatusPhrase);
+    }
+    output.write(Protocol.endOfLine);
     output.flush();
   }
 
   public void disconnect() throws IOException {
-    output.write(Protocol.disconnectionCommandPhrase + Protocol.endOfLine);
+    output.write(Protocol.disconnectionCommandPhrase);
+    output.write(Protocol.endOfLine);
     output.flush();
   }
 
@@ -77,10 +83,21 @@ class Connection {
   }
 
   public void sendMessage(String message) throws IOException {
-    output.write(Protocol.messageCommandPhrase + Protocol.endOfLine);
+    output.write(Protocol.messageCommandPhrase);
+    output.write(Protocol.endOfLine);
     output.write(message);
     if (! message.endsWith(Protocol.endOfLine))
       output.write(Protocol.endOfLine);
+    output.flush();
+  }
+  
+  public void sendFile(String title, byte[] rawData) throws IOException {
+    output.write(Protocol.fileCommandPhrase);
+    output.write(Protocol.endOfLine);
+    output.write(title);
+    output.write(Protocol.endOfLine);
+    output.write(new String(Base64.getUrlEncoder().encode(rawData)));
+    output.write(Protocol.endOfLine);
     output.flush();
   }
 }
